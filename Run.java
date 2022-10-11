69d9e5cca9b6cf08837ab36428453d5e55debe64
Joshua Cohen <jcohen02@terpmail.umd.edu>
2022-10-11 09:58:05 -0400

    Add files via upload

---
+++ Run.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+import Building.FloorLayoutGui;
+
+public class Run {
+
+	public static void main(String[] args) throws IOException {
+
+		new FloorLayoutGui();
+		
+	}
+
+}
